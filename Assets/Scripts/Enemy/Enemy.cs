8be4f0b92928c9ae8107c126448a8b5b5ab267c9
John Tai <zapyre_1@hotmail.com>
2011-08-19 07:22:28 -0400

    Updated EnemyPrototype to a cs file and optimized using ArrayList calls Updated RollingButton to contain capitals for functions Updated GameManager to now be called Main Manager and moved it to the "Main" Folder
    Signed-off-by: John Tai <zapyre_1@hotmail.com>

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,193 @@
+using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	
+	private string ename;
+	private Vector3 position;
+	private int maxHP, curHP;
+	private int moveSpeed;
+	private int armour;
+	// Typing:
+	// immunityList : ArrayList <String>
+	// timeZone : ArrayList <String>
+	// debuffList : ArrayList <String>
+	private ArrayList immunityList;
+	private ArrayList timeZoneList;
+	private ArrayList debuffList;
+	
+	public Enemy (string n, int x, int z, int hp, int ms, int arm, ArrayList imList, ArrayList tZone){
+		ename = n;
+		position = new Vector3 (x,0,z);
+		maxHP = hp;
+		curHP = hp;
+		moveSpeed = ms;
+		
+		if (arm > 100){
+			armour = 100;
+		}
+		else {
+			armour = arm;
+		}
+		
+		immunityList = new ArrayList ();
+		if (imList != null){
+			immunityList = imList;
+		}
+		
+		timeZoneList = new ArrayList ();
+		if (tZone != null){
+			timeZoneList = tZone;
+		}
+		
+		debuffList = new ArrayList ();
+	}
+	
+	// Getters and Setters
+	public string GetName(){
+		return ename;
+	}
+	public void SetName(string n){
+		ename = n;
+	}
+	public Vector3 GetPosition (){
+		return position;
+	}
+	public int GetPositionX (){
+		return (int) position.x;
+	}
+	public int GetPositionZ (){
+		return (int) position.z;
+	}
+	public void SetPositionVec (Vector3 p){
+		position = p;
+	}
+	public void SetPosition (int x, int z){
+		position.x = x;
+		position.z = z;
+	}
+	
+	public int GetMaxHP (){
+		return maxHP;
+	}
+	public void SetMaxHP(int mHP){
+		maxHP = mHP;
+	}
+	
+	public int GetCurHP (){
+		return curHP;
+	}
+	public void SetCurHP (int cHP){
+		curHP = cHP;
+	}
+	
+	public int GetMoveSpeed (){
+		return moveSpeed;
+	}
+	public void SetMoveSpeed (int ms){
+		moveSpeed = ms;
+	}
+	
+	public int GetArmour (){
+		return armour;
+	}
+	public void SetArmour (int arm){
+		if (arm > 100){
+			armour = 100;
+		}
+		else {
+			armour = arm;
+		}
+	}
+	
+	// ArrayList getters and Modifiers
+	// -- ImmunityList -- //
+	// Returns ArrayList <String>
+	public ArrayList GetImmunityList (){
+		return immunityList;
+	}
+	// Returns true if the immunity exists in the list, false ow
+	public bool ContainsImmunity (string imType){
+		if (immunityList.IndexOf(imType) != -1){
+			return true;
+		}
+		return false;
+	}
+	// Returns true if an Immunity is added, false ow
+	public bool AddImmunity (string imType){
+		if (ContainsImmunity(imType)){
+			return false;
+		}
+		immunityList.Add(imType);
+		return true;
+	}
+	// Returns true if an Immunity was removed, false ow
+	public bool RemoveImmunity (string imType){
+		int temp = immunityList.Count;
+		immunityList.Remove(imType);
+		if (immunityList.Count == temp){
+			return false;
+		}
+		return true;
+	}
+	
+	// -- TimeZone -- //
+	// Returns ArrayList <String>
+	public ArrayList GetTimeZoneList (){
+		return timeZoneList;
+	}
+	
+	public bool ContainsTimeZone (string tzType){
+		if (timeZoneList.IndexOf(tzType) != -1){
+			return true;
+		}
+		return false;
+	}
+	
+	public bool AddTimeZone (string tzType){
+		if (ContainsTimeZone(tzType)){
+			return false;
+		}
+		timeZoneList.Add(tzType);
+		return true;
+	}
+	
+	public bool RemoveTimeZone (string tzType){
+		int temp = timeZoneList.Count;
+		timeZoneList.Remove(tzType);
+		if (timeZoneList.Count == temp){
+			return false;
+		}
+		return true;
+	}
+	
+	// -- DebuffList -- //
+	// Returns ArrayList <String>
+	public ArrayList GetDebuffList (){
+		return debuffList;
+	}
+	
+	public bool ContainsDebuff (string dbType){
+		if (debuffList.IndexOf(dbType) != -1){
+			return true;
+		}
+		return false;
+	}
+	
+	public bool AddDebuff (string dbType){
+		if (ContainsDebuff(dbType)){
+			return false;
+		}
+		debuffList.Add(dbType);
+		return true;
+	}
+	
+	public bool RemoveDebuff (string dbType){
+		int temp = debuffList.Count;
+		debuffList.Remove(dbType);
+		if (debuffList.Count == temp){
+			return false;
+		}
+		return true;
+	}
+}
--- Assets/Scripts/GUI/Rolling Menu/RollingButton.cs
+++ Assets/Scripts/GUI/Rolling Menu/RollingButton.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class rollingButton : MonoBehaviour {
+public class RollingButton : MonoBehaviour {
 
 	public Texture gearSkin;
 	public Texture templateSkin;
@@ -18,7 +18,7 @@ public class rollingButton : MonoBehaviour {
 	public Texture infoSkinEarth;
 	public Texture infoSkinWind;
 	public GUIStyle infoTextStyle;
-	
+
 	private Texture infoSkin;
 	private bool mouseInfo;
 	private string towerInfo;
@@ -26,7 +26,7 @@ public class rollingButton : MonoBehaviour {
 	private int animateX;
 	private int animateSpeed;
 	private Vector2 pivotPoint;
-	
+
 	private int width;
 	private int height;
 	private int winfo;
@@ -36,14 +36,14 @@ public class rollingButton : MonoBehaviour {
 	private int spacing;
 	private int menuWidth;
 	private int menuHeight;
-	
+
 	private int gear1X;
 	private int gear1Y;
 	private int gear1Size;
 	private int gear2X;
 	private int gear2Y;
 	private int gear2Size;
-	
+
 	// Use this for initialization
 	void Start () {
 		infoSkin = infoSkinWind;
@@ -52,20 +52,20 @@ public class rollingButton : MonoBehaviour {
 		infoMsg = "";
 		animateX = 0;
 		animateSpeed = 2;
-		
+
 		width = 100;
 		height = 90;
 		// Note that animation time depends on the width of the info box
 		winfo = height * 2;
 		hinfo = height * 2;
-		
+
 		spacing = 10;
 		x = Screen.width-width*2-spacing*2;
 		y = Screen.height-height*2-spacing*2;
-		
+
 		menuWidth = width*2 + spacing*3;
 		menuHeight = height*2 + spacing*3;
-		
+
 		gear1X = x - spacing-width/4;
 		gear1Y = y - spacing-width/4;
 		gear1Size = width/2;
@@ -73,7 +73,7 @@ public class rollingButton : MonoBehaviour {
 		gear2Y = y + menuHeight - width/4 - spacing;
 		gear2Size = width/2;
 	}
-	
+
 	void OnGUI (){
 		if (animateX > 0){
 			if (animateX - animateSpeed < 0){
@@ -83,7 +83,7 @@ public class rollingButton : MonoBehaviour {
 				animateX -= animateSpeed;
 			}
 		}
-		
+
 		if (mouseInfo){
 			GUI.DrawTexture(new Rect(x-(winfo-animateX),y,winfo+spacing,hinfo+spacing),infoSkin);
 			// Text Label
@@ -94,51 +94,51 @@ public class rollingButton : MonoBehaviour {
 				// Create Tower of type x here
 			}
 		}
-		
+
 		// Frame Texture
 		GUI.DrawTexture(new Rect(x-spacing, y-spacing, menuWidth, menuHeight),templateSkin);
-			
+
 		// Button Creation
 		GUI.skin = fireSkin;
 		if (GUI.Button (new Rect(x,y,width,height),"")){
-			setHelper("Fire");
+			SetHelper("Fire");
 		}
 		GUI.DrawTexture(new Rect(x,y,width,height),fireDecal);
-		
+
 		GUI.skin = iceSkin;
 		if (GUI.Button (new Rect(x + width + spacing,y,width,height),"")){
-			setHelper("Ice");
+			SetHelper("Ice");
 		}
 		GUI.DrawTexture(new Rect(x + width + spacing,y,width,height),iceDecal);
-		
+
 		GUI.skin = earthSkin;
 		if (GUI.Button (new Rect(x,y + height + spacing,width,height),"")){
-			setHelper("Earth");
+			SetHelper("Earth");
 		}
 		GUI.DrawTexture(new Rect(x,y + height + spacing,width,height),earthDecal);
-		
+
 		GUI.skin = windSkin;
 		if (GUI.Button (new Rect(x + width + spacing,y + height + spacing,width,height),"")){
-			setHelper("Wind");
+			SetHelper("Wind");
 		}
 		GUI.DrawTexture(new Rect(x + width + spacing,y + height + spacing,width,height),windDecal);
-		
+
 		// Gear Texture
-		
-		
+
+
 		pivotPoint = new Vector2(x-spacing, y-spacing);
 		GUIUtility.RotateAroundPivot (-animateX*2, pivotPoint); 
 		GUI.DrawTexture(new Rect(gear1X, gear1Y, gear1Size, gear1Size),gearSkin);
 		GUIUtility.RotateAroundPivot (animateX*2, pivotPoint); 
-		
+
 		pivotPoint = new Vector2(x - spacing, y + menuHeight - spacing);
 		GUIUtility.RotateAroundPivot (animateX*2, pivotPoint); 
 		GUI.DrawTexture(new Rect(gear2X, gear2Y,gear2Size,gear2Size),gearSkin);
 		GUIUtility.RotateAroundPivot (-animateX*2, pivotPoint);
 	}
-	
+
 	// Helper function to set the info window in motion
-	private void setHelper (string towerType){
+	private void SetHelper (string towerType){
 		mouseInfo = true;
 		towerInfo = towerType;
 		animateX = winfo;
